b1a9ae330b12cdee11437bdac8686b47fc221ba2
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2021-02-15 12:58:02 +0300

    feat: создать роут, контроллер, класс ошибки для обновления данных пользователя put /users/me

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 
+const BadRequestError = require('../errors/BadRequestError');
+
 const getCurrentUser = (req, res, next) => {
 
   const id = req.user._id;
@@ -14,6 +16,28 @@ const getCurrentUser = (req, res, next) => {
     .catch(next);
 };
 
+const updateCurrentUser = (req, res, next) => {
+  const id = req.user._id;
+  const newName = req.body.name;
+  const newEmail = req.body.email;
+
+  User.findOneAndUpdate(
+    { _id: id },
+    { name: newName, email: newEmail },
+    { runValidators: true, new: true },
+  )
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        throw new BadRequestError(err.message);
+      }
+    })
+    .catch(next);
+}
+
 module.exports = {
-  getCurrentUser
+  getCurrentUser,
+  updateCurrentUser,
 };
