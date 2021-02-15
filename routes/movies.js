f2f106aa969f3d65759c6eee084374e85342a603
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2021-02-15 17:16:46 +0300

    feat: создать роут и контроллер для delete /movies/:movieId

--- routes/movies.js
+++ routes/movies.js
@@ -3,14 +3,17 @@ const movieRouter = require('express').Router();
 const {
   validateId,
   validateCreateMovie,
+  validateDeleteMovie,
 } = require('../middlewares/requestValidation');
 
 const {
   getMovies,
   createMovie,
+  deleteMovie,
 } = require('../controllers/movies');
 
 movieRouter.get('/movies', validateId, getMovies);
 movieRouter.post('/movies', validateCreateMovie,createMovie);
+movieRouter.delete('/movies/:movieId', validateDeleteMovie, deleteMovie);
 
 module.exports = movieRouter;
--- utils/constants.js
+++ utils/constants.js
@@ -1,9 +1,14 @@
 const BAD_REQUEST = 'Неверно переданы данные';
 const INTERNAL_SERVER_ERROR = 'На сервере произошла ошибка';
 const BAD_URL = 'не является URL адресом';
-
+const NOT_FOUND_MESSAGE = 'Нет фильма с таким id';
+const VALIDATION_ERROR_NAME = 'ValidationError';
+const FORBIDDEN_DELETE_MOVIE_MESSAGE = 'Нет доступа к удалению фильма';
 module.exports = {
   BAD_REQUEST,
   INTERNAL_SERVER_ERROR,
   BAD_URL,
+  NOT_FOUND_MESSAGE,
+  VALIDATION_ERROR_NAME,
+  FORBIDDEN_DELETE_MOVIE_MESSAGE,
 }
